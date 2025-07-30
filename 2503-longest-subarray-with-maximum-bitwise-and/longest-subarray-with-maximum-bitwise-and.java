51730b089eae0fb4c141f2bd116c0956702db4a5
Utkarsh Tiwari <153006721+utkxrsh13@users.noreply.github.com>
2025-07-30 09:56:16 +0530

    Time: 3 ms (82.57%) | Memory: 61.2 MB (21.99%) - LeetSync

---
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int maxAnd = 0;
+        
+        // Step 1: Find max bitwise AND possible (which is the max number itself)
+        for (int num : nums) {
+            maxAnd = Math.max(maxAnd, num);
+        }
+
+        int longest = 0;
+        int currentLen = 0;
+        int currentAnd = -1;
+
+        // Step 2: Traverse and simulate ANDs
+        for (int num : nums) {
+            if (num == maxAnd) {
+                currentLen++;
+                longest = Math.max(longest, currentLen);
+            } else {
+                currentLen = 0;
+            }
+        }
+
+        return longest;
+    }
+}
