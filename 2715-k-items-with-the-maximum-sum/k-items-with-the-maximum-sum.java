26ca7f414e09b7412e1f1de590c37637634b9669
Utkarsh Tiwari <153006721+utkxrsh13@users.noreply.github.com>
2025-09-09 20:43:41 +0530

    Time: 1 ms (87.35%) | Memory: 41 MB (82.75%) - LeetSync

--- 2715-k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.java
+++ 2715-k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.java
@@ -1,8 +1,8 @@
 class Solution {
     public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
-        if(k<=numOnes) return k;
+        if(numOnes>=k) return k;
 
-        if(k<= numOnes+ numZeros) return numOnes;
+        if( numOnes+ numZeros>=k) return numOnes;
 
         return numOnes-(k-numOnes-numZeros);
     }
